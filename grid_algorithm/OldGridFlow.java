fa0483f85e12f4e14fe1b7bf8660bdbe59dc7601
Krokant <ga63sam@mytum.de>
2022-09-26 17:17:51 +0200

    Removed License Template Headers.

--- grid_algorithm/OldGridFlow.java
+++ grid_algorithm/OldGridFlow.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package grid_algorithm;
 
 import java.util.HashMap;
@@ -14,7 +9,7 @@ import project_utils.Tuple;
 
 /**
  *
- * @author kroka
+ * @author Jonas Schulz
  */
 @Deprecated
 public class OldGridFlow implements Iterable<Triple<Integer,Integer,Double>>{
